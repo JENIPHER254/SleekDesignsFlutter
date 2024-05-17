278952a7fe2d7bf58e0871e98255312329c039fd
JENIPHER <jeniperkuki@gmail.com>
2024-05-17 15:18:54 +0300

    large text widget

---
+++ pizza_pal/lib/widgets/large_text/large_text.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Text large_text(String text) {
+  return Text(
+    text,
+    style: TextStyle(
+      color: Colors.black,
+      fontSize: 24,
+      fontWeight: FontWeight.bold
+    ),
+  );
+}
