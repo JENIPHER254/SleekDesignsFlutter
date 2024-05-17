9ba07610c72505ad1bced531a31bfadc9f5f5eaf
JENIPHER <jeniperkuki@gmail.com>
2024-05-17 12:35:59 +0300

    route setup

--- pizza_pal/test/widget_test.dart
+++ pizza_pal/test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:pizza_pal/screens/welcome_screen.dart';
 
-import 'package:pizza_pal/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const WelcomeScreen ());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
